0e450d15d7dae6df9d5e32af3abbc42b064194fc
XKK <1538890100@qq.com>
2021-02-23 16:09:12 +0800

    lasthomework

---
+++ lasthomework/submit.js
@@ -0,0 +1,53 @@
+const express=require('express')
+const app=express()
+const axios=require('axios')
+const fs=require('fs')
+const bodyParser = require("body-parser")
+const { count } = require('console')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(5050,()=>{
+    console.log('index listening on port 5050!')
+})
+var times=''
+var item=0
+var count1=0
+app.post('/index',(request,response)=>{
+    console.log(request.body)
+    var data={
+        schoolYear: '2020',
+        studentNo: request.body.studentNo
+    }
+    axios.post('https://tyxsjpt.seu.edu.cn/api/exercise/morning/attendance/get-by-student',data)
+    .then(res => {
+        for(item in res.data.data){
+            times+=`<tr><td>第${item}次跑操时间</td><td>${res.data.data[item].recordTime}</td></tr>`
+        }
+        count1=Object.keys(res.data.data).length
+    })
+    .catch(error => {
+        console.error(error)
+        })
+        var datas={
+            schoolYear: '2021',
+            studentNo: request.body.studentNo
+        }
+    axios.post('https://tyxsjpt.seu.edu.cn/api/exercise/morning/attendance/get-by-student',datas)
+        .then(res => {
+            for(var items in res.data.data){
+                var itemss=parseInt(items)+parseInt(item)+1
+                times+=`<tr><td>第${itemss}次跑操时间</td><td>${res.data.data[items].recordTime}</td></tr>`
+            }
+            let counts=Object.keys(res.data.data).length+count1
+            fs.readFile('./result.html',(err,data)=>{
+                if(err) throw err
+                else{
+                    response.send(data.toString().replace('@@@Time@@@',times).replace('@@@count@@@',counts.toString()))
+                }
+            })
+        })
+        .catch(error => {
+            console.error(error)
+        })
+})
